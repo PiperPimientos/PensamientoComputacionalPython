e94d1c4de867d6a86b28f4958b4a5a5cad342d4a
felipe <feliperestrepomeneses@gmail.com>
2021-10-12 00:41:22 -0500

    Terminamos nuestro programa para encontrar raices cuadradas encapsulando los algoritmos de brute force, approximation y binary search, en funciones

--- function.py
+++ function.py
@@ -95,15 +95,18 @@ def binary_search():
     epsilon = 0.01
     bajo = 0.0
     alto = max(1.0, objetivo)
-    respuesta = (alto + bajo) / 2
+    mid = (alto + bajo) / 2
     
-    while abs(respuesta**2 - objetivo) >= epsilon:
-        print(f'bajo={bajo}, alto={alto}, respuesta={respuesta}')
-        if respuesta**2 < objetivo:
-            bajo = respuesta
+    while abs(mid**2 - objetivo) >= epsilon:
+        print(f'bajo={bajo}, alto={alto}, mid={mid}')
+        if mid**2 < objetivo:
+            bajo = mid
         else:
-            alto = respuesta
+            alto = mid
     
-        respuesta = (alto + bajo) / 2
+        mid = (alto + bajo) / 2
     
-    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+    print(f'La raiz cuadrada de {objetivo} es {mid}')
+
+
+algorithms()
