b08fcc181fdc1bb6ff77547f855ba898a604b017
felipe <feliperestrepomeneses@gmail.com>
2021-10-14 00:47:52 -0500

    agregamos tuplas.py donde vemos como funciona esta primer estructura de datos

---
+++ tuplas.py
@@ -0,0 +1,25 @@
+# Es el primero tipo de estructura de dato.
+
+# Las tuplas son secuencias inmutables de objetos. A diferencia de las cadenas, pueden contener cualquier tipo de objeto. Puede utilizarse para devolver varios valores en una función.
+
+# Las tuplas al igual que las listas en Python son secuencias pero las tuplas son secuencias inmutables (eso significa que no se pueden modificar) de objetos.
+
+# ¿Cómo identificar una tupla en Python?
+# Las tuplas en Pyhton poseen siguiente sintaxis:
+tuple_name = ('first_value', 2, 'u_can_add_ints')
+# Mientras que las listas en Python tienen esta sintaxis:
+# list_name = [1,'2', 3]
+# Como pueden apreciar, para la asignación de las tuplas, el contenido debe ir “empaquetado” mediante paréntesis “()”, mientras que para las listas, se empaquetan en corchetes “[]”.
+
+# Para declarar una tupla en Python se realiza lo siguiente:
+tuple_name = ()
+# El contenido de la tupla pueden ser de tipo int, float y string.
+
+# Nota: en caso de que tu tupla sólo contenga un valor, debes agregar una comar “,”, por ejemplo:
+
+# En caso de contener un int:
+# tuple_name = (1,) 
+# En caso de contener un float:
+# tuple_name = (2.0,) 
+# En caso de contener un string:
+# tuple_name = ('string',) 
